0a0048265f31782b2d7a52051671efa45941408b
Fenix Sampson <Fenix.sampson@gmail.com>
2021-09-30 00:11:01 -0400

    modified some code trying to get save buttons to appear correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,53 @@
-$(document).ready(function() {
-    //displays current date and time
-    $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+$(document).ready(function () {
+    //displays current day & time.
+    $("#currentDay").text(moment().format("LLLL"));
 
-//assiging save button to click listener
-$('.saveBtn').on('click', function (){
-    console.log("test")
-    var text = $(this).siblings('.description').val();
-    var time = $(this).parent().attr('id');
-    console.log(text);
-    console.log(time);
-    //setting items to local storage
-    localStorage.setItem(time, text);
-})
+    //Assign saveBtn on click listener 
+    $(".saveBtn").on("click", function () {
+        //get nearby values.
+        console.log(this);
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
 
-//loads saved data from local storage for each hour
-$('#hour-9' .description).val(localStorage.getItem('hour-9'));
-$('#hour-10' .description).val(localStorage.getItem('hour-10'));
-$('#hour-11' .description).val(localStorage.getItem('hour-11'));
-$('#hour-12' .description).val(localStorage.getItem('hour-12'));
-$('#hour-13' .description).val(localStorage.getItem('hour-13'));
-$('#hour-14' .description).val(localStorage.getItem('hour-14'));
-$('#hour-15' .description).val(localStorage.getItem('hour-15'));
-$('#hour-16' .description).val(localStorage.getItem('hour-16'));
-$('#hour-17' .description).val(localStorage.getItem('hour-17'));
+        //set items in local storage.
+        localStorage.setItem(time, text);
+    })
+//load any saved data from LocalStorage - do this for each hour created.
+$("#hour-9 .description").val(localStorage.getItem("hour9"));
+$("#hour-10 .description").val(localStorage.getItem("hour10"));
+$("#hour-11 .description").val(localStorage.getItem("hour11"));
+$("#hour-12 .description").val(localStorage.getItem("hour12"));
+$("#hour-13 .description").val(localStorage.getItem("hour13"));
+$("#hour-14 .description").val(localStorage.getItem("hour14"));
+$("#hour-15 .description").val(localStorage.getItem("hour15"));
+$("#hour-16 .description").val(localStorage.getItem("hour16"));
+$("#hour-17 .description").val(localStorage.getItem("hour17"));
 
-function hourTracker () {
-    //gets current hour number
+
+function hourTracker() {
+    //get current hour number
     let currentHour = moment().hour();
-    //creating loop to go over time block
-    $ ('.time-block').each(function(){
-        let blockHour = parseInt($(this).attr('id').split('hour')[1]);
-        console.log(blockHour, currentHour)
-
-    } )
-
-
+    // loop over time blocks
+    $(".time-block").each(function () {
+    let blockHour = parseInt($(this).attr("id").split("hour")[1]);
+    console.log( blockHour, currentHour)
+    //check to see if we move past the time on the planner
+    if (blockHour < currentHour) {
+        $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+    }
+    else if (blockHour === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+    }
+    else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+    } 
+    })
 }
-
-
-
-
-
+    hourTracker();
 })
