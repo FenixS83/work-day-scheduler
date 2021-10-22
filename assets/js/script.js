40dbde9e207a9064a066808d40b434121a077182
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-22 16:34:01 -0400

    started working on correcting errors with work-day-scheduler

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,46 @@
 $(document).ready(function () {
     //this section applies the current date and time to the header
-    $("#currentDay").text(moment().format("LLLL"));
-
-    //this function assigns a click listener to the save button 
     $(".saveBtn").on("click", function () {
-        //get nearby values.
-        console.log(this);
-        let text = $(this).siblings(".description").val();
-        let time = $(this).parent().attr("id");
-
-        //set items in local storage.
-        localStorage.setItem(time, text);
+        var value = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, value);
+        console.log(time, value);
     })
-//This section will look for any local storage data that would apply for any of the applicable hours 
+    
+    //This section will look for any local storage data that would apply for any of the applicable hours 
+    $("#hour-9").children("textarea").val(localStorage.getItem("hour-9"));
+    $("#hour-10").children("textarea").val(localStorage.getItem("hour-10"));
+    $("#hour-11").children("textarea").val(localStorage.getItem("hour-11"));
+    $("#hour-12").children("textarea").val(localStorage.getItem("hour-12"));
+    $("#hour-13").children("textarea").val(localStorage.getItem("hour-13"));
+    $("#hour-14").children("textarea").val(localStorage.getItem("hour-14"));
+    $("#hour-15").children("textarea").val(localStorage.getItem("hour-15"));
+    $("#hour-16").children("textarea").val(localStorage.getItem("hour-16"));
+    $("#hour-17").children("textarea").val(localStorage.getItem("hour-17"));
 
-$("#hour-9 .description").val(localStorage.getItem("hour-9"));
-$("#hour-10 .description").val(localStorage.getItem("hour-10"));
-$("#hour-11 .description").val(localStorage.getItem("hour-11"));
-$("#hour-12 .description").val(localStorage.getItem("hour-12"));
-$("#hour-13 .description").val(localStorage.getItem("hour-13"));
-$("#hour-14 .description").val(localStorage.getItem("hour-14"));
-$("#hour-15 .description").val(localStorage.getItem("hour-15"));
-$("#hour-16 .description").val(localStorage.getItem("hour-16"));
-$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-function hourTracker() {
-    //this function will retreive the current time
-    let currentHour = moment().hour();
-    // loop over time blocks
-    $(".time-block").each(function () {
-    let blockHour = parseInt($(this).attr("id").split("hour")[1]);
-    console.log( blockHour, currentHour)
-    //this section will compare the scheduler to the current time, adjusting display properties as time progresses
-    if (blockHour < currentHour) {
-        $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
-    }
-    else if (blockHour === currentHour) {
-        $(this).removeClass("past");
-        $(this).addClass("present");
-        $(this).removeClass("future");
-    }
-    else {
-        $(this).removeClass("present");
-        $(this).removeClass("past");
-        $(this).addClass("future");
-    } 
+    
+    var currentDate = moment();
+    $("#currentDay").text(currentDate.format("dddd, MMMM Do"));
+
+
+    //Updates background color based off of time of day
+    var timeofDay = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
+    updateTime();
+    
+        function updateTime() {
+            var currentTime = moment().format("H");
+            for(var i=0; i < timeofDay.length; i++) {
+                //green bg color
+                if (parseInt(timeofDay[i]) > currentTime) {
+                    $("#" + timeofDay[i]).attr("style", "background-color: #58ce7b");
+                //grey bg color
+                } else if (parseInt(timeofDay[i]) < currentTime) {
+                    $("#" + timeofDay[i]).attr("style", "background-color: lightgray");
+                //red bg color
+                } else if (parseInt(timeofDay[i]) == currentTime) {
+                    $("#" + timeofDay[i]).attr("style", "background-color: #fc665e");
+                }
+            }
+        }
     })
-}
-    hourTracker();
-})
-
